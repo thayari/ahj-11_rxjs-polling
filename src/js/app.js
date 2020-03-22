fc2df6abb22add5759d3170c3307c2069f1840fd
thayari <alex.silvanovich@gmail.com>
2020-03-22 21:12:13 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,40 @@
 import { ajax } from 'rxjs/ajax';
-import { map, catchError } from 'rxjs/operators';
-import { of, from, Subject, interval } from 'rxjs';
+import { map, catchError, concatMap } from 'rxjs/operators';
+import { of, from, Subject, interval, range, Observable } from 'rxjs';
 
+//const url = 'http://localhost:7070/messages/unread';
 const url = 'https://ahj-11-rxjs-polling-server.herokuapp.com/messages/unread';
 
-// const url = 'http://localhost:7070/messages/unread';
 
-//const stream$ = new Subject();
+function getRequest(url) {
+  return new Observable(observer => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        observer.next(data);
+        observer.complete();
+      })
+      .catch(err => observer.error(err));
+    return () => controller.abort();
+  });
+}
 
+interval(1000)
+  .pipe(concatMap((i) => getRequest(url)))
+  .subscribe(value => console.log(value));
+
+/*
 const obs$ = ajax({
-  url: url,
-  method: 'GET'
+url: url,
+method: 'GET'
 }).pipe(
-  map(value => value.response.messages),
-  catchError(error => {
-    console.log('error: ', error);
-    return of(error);
-  })
+map(value => value.response.messages),
+catchError(error => {
+  console.log('error: ', error);
+  return of(error);
+})
 );
 
 
 from(obs$).subscribe(value => console.log(value));
+*/
